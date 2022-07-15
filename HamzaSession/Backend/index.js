07ea028ba40e6539405b4efa35cae3f77057d90f
maroaraujo <tiagomoller@tiagos-mbp.home>
2022-07-15 10:02:59 +0100

    Inicialize the folders and backend is listing PORT 5000

--- HamzaSession/Backend/index.js
+++ HamzaSession/Backend/index.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = 5000;
 import cors from "cors";
 
+// CORS is used to connect the information from backend to frontend
+app.use(cors());
+// Unpack any json request, normally for POST,PUT
 app.use(express.json());
+
 app.get("/", (req, res)=>{
     res.json({success: "true", payload: "get all them for you!"})
 })
